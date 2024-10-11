d14b7b3a38bc7f4c9b1dc27765511f48a80a5ada
re-ovo <qq1609403959@outlook.com>
2024-10-11 17:28:54 +0800

    init

--- src/main/kotlin/me/rerere/wgslplugin/lang/WGSLLanguage.kt
+++ src/main/kotlin/me/rerere/wgslplugin/lang/WGSLLanguage.kt
@@ -1,4 +1,19 @@
 package me.rerere.wgslplugin.lang
 
-class WGSLLanguage {
+import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
+
+
+object WGSLLanguage : Language("WGSL")
+
+object WGSLIcons {
+    val FILE = IconLoader.getIcon("/icons/wgsl.png", WGSLIcons::class.java);
+}
+
+object WGSLLanguageFileType : LanguageFileType(WGSLLanguage) {
+    override fun getName(): String = "WGSL"
+    override fun getDescription(): String = "WebGPU Shading Language"
+    override fun getDefaultExtension(): String = "wgsl"
+    override fun getIcon() = WGSLIcons.FILE
 }
